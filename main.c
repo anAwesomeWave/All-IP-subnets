0cde846bec232f673284a3e5d00f97e836c8b2e6
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-03-19 21:24:28 +0300

    Реализовал преобразование строки в int.

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     if(argc == 2) {
         int subnet= 32;
         char* s = strtok(argv[1], ".");
+        int partsOfIp = 4;
+        int arr[4];
+        int cnt = 0;
         while(s != NULL) {
-            printf("%s\n", s);
+            arr[cnt ++] =  atoi(s);
+            printf("%d\n", arr[cnt - 1]);
             s = strtok(NULL, ".");
         }
     } else {
