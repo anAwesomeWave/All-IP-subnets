fd4b39f19615f92cadd663ec1b446ad9d1468726
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-03-19 21:32:28 +0300

    Correct error handling

--- main.c
+++ main.c
@@ -9,13 +9,22 @@ int main(int argc, char* argv[]) {
         int partsOfIp = 4;
         int arr[4];
         int cnt = 0;
+        int error = 0;
         while(s != NULL) {
             arr[cnt ++] =  atoi(s);
             printf("%d\n", arr[cnt - 1]);
+            if(arr[cnt-1] < 0 || arr[cnt-1] > 255) {
+                error = 1;
+            }
             s = strtok(NULL, ".");
         }
+        if(cnt != 4 || error) {
+            printf("ERROR: Wrong format of string\nExpected x.x.x.x, where 0 <= x <= 255\n");
+        }else {
+            printf("OK");
+        }
     } else {
-        printf("Wrong number of args\nExpected: 1, got %d", argc-1);
+        printf("ERROR:Wrong number of args\nExpected: 1, got %d\n", argc-1);
     }
     return 0;
 }
