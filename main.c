fdf7b82c052d9f0237dccc3395d25b538821f044
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-03-19 21:14:20 +0300

    Написал разделение на лексемы.

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 
 int main(int argc, char* argv[]) {
     if(argc == 2) {
-
+        int subnet= 32;
+        char* s = strtok(argv[1], ".");
+        while(s != NULL) {
+            printf("%s\n", s);
+            s = strtok(NULL, ".");
+        }
     } else {
         printf("Wrong number of args\nExpected: 1, got %d", argc-1);
     }
