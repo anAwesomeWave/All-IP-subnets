6544cb5dac60e67e7740b4de09d0139ac60c0018
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-03-19 21:12:36 +0300

    Оформил ввод.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    if(argc == 2) {
+
+    } else {
+        printf("Wrong number of args\nExpected: 1, got %d", argc-1);
+    }
+    return 0;
+}
